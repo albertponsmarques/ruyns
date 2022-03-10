465d11406bf36cafadac7b7d5ba7903cead6af87
albert.pons.7e4 <albert.pons.7e4@itb.cat>
2022-03-10 19:05:06 +0100

    algo es algo

--- app/src/main/java/com/example/ruyns/MapFragment.kt
+++ app/src/main/java/com/example/ruyns/MapFragment.kt
@@ -1,54 +1,29 @@
 package com.example.ruyns
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.View.inflate
-import android.view.ViewGroup
-import androidx.core.content.res.ColorStateListInflaterCompat.inflate
-import com.example.ruyns.databinding.ActivityMapsBinding
-import com.example.ruyns.databinding.ActivityMapsBinding.inflate
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
-import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 
 class MapFragment : Fragment() {
     private lateinit var viewModel: ViewModel
     private lateinit var mMap: GoogleMap
-    private lateinit var binding: ActivityMapsBinding
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding = MapFragment.inflate(LayoutInflater,container,false)
-        setContentView(binding.root)
 
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        val mapFragment = supportFragmentManager
-            .findFragmentById(R.id.map) as SupportMapFragment
-        mapFragment.getMapAsync(this)
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
-    override fun onMapReady(googleMap: GoogleMap) {
+    fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
 
         // Add a marker in Sydney and move the camera
         val sydney = LatLng(-34.0, 151.0)
         mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
-    }
     }
 }
